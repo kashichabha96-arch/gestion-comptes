0c3c8ed3046630d99aae0187035c38ceba4e9d40
kashichabha96-arch <kashichabha96@gmail.com>
2026-06-02 12:55:34 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,10 +2,19 @@
 
 echo "🚀 Laravel starting..."
 
+# Clear old cache (important on deploy)
+php artisan optimize:clear
+
+# Rebuild cache (safe for production)
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
+# Run migrations
 php artisan migrate --force
 
+# ✅ RUN SEEDERS (IMPORTANT FIX)
+php artisan db:seed --force
+
+# Start Apache
 apache2-foreground
