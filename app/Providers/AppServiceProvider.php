568fb0f0cf6c6dd4dc8376785c10fe5c43b21bfd
kashichabha96-arch <kashichabha96@gmail.com>
2026-06-02 12:17:41 +0100

    Update boot method to conditionally enforce HTTPS
    Ensure HTTPS scheme is enforced only in production environment.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,11 +15,10 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
+    public function boot()
     {
-    URL::forceScheme('https');
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
+    
     }
-}
