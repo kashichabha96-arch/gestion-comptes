bce191a7210ba5d60b17d5a7fc24d8e6f297b389
kashichabha96-arch <kashichabha96@gmail.com>
2026-06-02 12:52:56 +0100

    Regenerate session on successful login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -23,7 +23,9 @@ class LoginController extends Controller
         $credentials = $request->only('email', 'password');
 
         if (Auth::attempt($credentials)) {
+
             $request->session()->regenerate();
+
             return redirect()->intended('/dashboard');
         }
 
